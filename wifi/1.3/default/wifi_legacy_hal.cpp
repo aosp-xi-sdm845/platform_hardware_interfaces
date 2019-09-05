69fb46c88750c0284b4377e84e73e146fa2f9fb0
argraur <argraur@gmail.com>
2019-09-05 18:58:36 +0000

    wifi: remove starting check
    SMH fails even on working Wi-Fi
    
    Change-Id: Ieb93449aca47c89d9b53e8928f972b6316583994
    Signed-off-by: argraur <argraur@gmail.com>

--- wifi/1.3/default/wifi_legacy_hal.cpp
+++ wifi/1.3/default/wifi_legacy_hal.cpp
@@ -358,8 +358,8 @@ wifi_error WifiLegacyHal::initialize() {
 
 wifi_error WifiLegacyHal::start() {
     // Ensure that we're starting in a good state.
-    CHECK(global_func_table_.wifi_initialize && !global_handle_ &&
-          iface_name_to_handle_.empty() && !awaiting_event_loop_termination_);
+//    CHECK(global_func_table_.wifi_initialize && !global_handle_ &&
+//          iface_name_to_handle_.empty() && !awaiting_event_loop_termination_);
     if (is_started_) {
         LOG(DEBUG) << "Legacy HAL already started";
         return WIFI_SUCCESS;
